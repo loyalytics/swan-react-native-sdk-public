e73d9ea01e477e6232119d78bba3db96eea5c5f9
LisahMan <lisahmanandhar1@gmail.com>
2024-12-09 16:18:33 +0530

    qa build

---
+++ lib/typescript/module/src/components/HeaderView.d.ts
@@ -0,0 +1,3 @@
+declare const HeaderView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
+export default HeaderView;
+//# sourceMappingURL=HeaderView.d.ts.map
