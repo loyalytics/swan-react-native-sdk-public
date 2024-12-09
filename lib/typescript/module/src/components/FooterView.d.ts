36f35989d713ae4111165f0a1fb41dc5e5e9fdc5
LisahMan <lisahmanandhar1@gmail.com>
2024-12-09 15:51:45 +0530

    Qa url

---
+++ lib/typescript/module/src/components/FooterView.d.ts
@@ -0,0 +1,3 @@
+declare const FooterNotification: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
+export default FooterNotification;
+//# sourceMappingURL=FooterView.d.ts.map
