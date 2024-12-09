091f8112768af50dabd51e278ac31f052b145059
LisahMan <lisahmanandhar1@gmail.com>
2024-12-09 16:17:37 +0530

    removed

--- lib/typescript/commonjs/src/components/FooterView.d.ts
+++
@@ -1,3 +0,0 @@
-declare const FooterNotification: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
-export default FooterNotification;
-//# sourceMappingURL=FooterView.d.ts.map
--- lib/typescript/commonjs/src/components/HeaderView.d.ts
+++
@@ -1,3 +0,0 @@
-declare const HeaderView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
-export default HeaderView;
-//# sourceMappingURL=HeaderView.d.ts.map
