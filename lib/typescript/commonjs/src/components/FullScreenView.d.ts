e260f9b5003437ed14d2a5aea3274c3695044800
LisahMan <lisahmanandhar1@gmail.com>
2025-01-17 16:12:57 +0530

    qa url change

---
+++ lib/typescript/commonjs/src/components/FullScreenView.d.ts
@@ -0,0 +1,3 @@
+declare const FullScreenView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
+export default FullScreenView;
+//# sourceMappingURL=FullScreenView.d.ts.map
