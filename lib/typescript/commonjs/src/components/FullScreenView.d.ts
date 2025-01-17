a2c174402b3a64e0575d04a21ed2b915dbd31ef5
LisahMan <lisahmanandhar1@gmail.com>
2025-01-17 16:12:41 +0530

    changed the qa url

--- lib/typescript/commonjs/src/components/FullScreenView.d.ts
+++
@@ -1,3 +0,0 @@
-declare const FullScreenView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
-export default FullScreenView;
-//# sourceMappingURL=FullScreenView.d.ts.map
--- lib/typescript/module/src/components/PopUpView.d.ts
+++
@@ -1,3 +0,0 @@
-declare const PopUpView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
-export default PopUpView;
-//# sourceMappingURL=PopUpView.d.ts.map
