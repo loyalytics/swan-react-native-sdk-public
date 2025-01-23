2f72229ffded0851793c8c6e3d9957592adf93b3
LisahMan <lisahmanandhar1@gmail.com>
2025-01-23 14:31:10 +0530

    production build

---
+++ lib/typescript/commonjs/src/components/PopUpView.d.ts
@@ -0,0 +1,3 @@
+declare const PopUpView: ({ designConfig, onClose, onButtonClick }: any) => import("react/jsx-runtime").JSX.Element;
+export default PopUpView;
+//# sourceMappingURL=PopUpView.d.ts.map
